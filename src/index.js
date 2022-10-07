4a50c927f75799655c1301c2ff60179326af080a
Yulia Lepner <ylepner@gmail.com>
2022-10-07 19:42:40 +0200

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openBrackets = []
+  let closeBrackets = []
+  let stackArr = []
+  bracketsConfig.forEach((arr) => {
+    openBrackets.push(arr[0]);
+    closeBrackets.push(arr[1])
+  })
+  for (let el of str) {
+    if (openBrackets.includes(el) && closeBrackets.includes(el)) {
+      const test3 = stackArr[stackArr.length - 1]
+      if (stackArr[stackArr.length - 1] === el) {
+        stackArr.pop()
+      } else {
+        stackArr.push(el)
+      }
+    }
+    else if (openBrackets.includes(el)) {
+      stackArr.push(el)
+    } else if (closeBrackets.includes(el)) {
+      if (stackArr.length === 0) {
+        return false
+      }
+      if (openBrackets.indexOf(stackArr[stackArr.length - 1]) === closeBrackets.indexOf(el)) {
+        stackArr.pop()
+      }
+    }
+  }
+  if (stackArr.length === 0) {
+    return true
+  }
+  return false
 }
